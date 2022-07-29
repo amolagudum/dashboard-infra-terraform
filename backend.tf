feccee9d0248f6365c1a1553e385282498a14771
amolagudum <96540958+amolagudum@users.noreply.github.com>
2022-07-29 21:39:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ctl-terraform-state-ap-south-1"
-    key            = "dashboard/asg-tf"
+    key            = "amol/demo"
     region         = "ap-south-1"
     dynamodb_table = "terraform-state"
 
